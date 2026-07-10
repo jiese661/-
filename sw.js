bd6f14ed62b8a2f45d5430e706edc38c0f10b379
jiese661 <18789278068@139.com>
2026-07-10 10:05:53 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,17 @@
-self.addEventListener('install', (event) => {
+// 强制清除旧缓存并更新到最新版本
+self.addEventListener('install', event => {
   self.skipWaiting();
 });
 
-self.addEventListener('activate', (event) => {
-  event.waitUntil(clients.claim());
+self.addEventListener('activate', event => {
+  // 清除所有旧缓存
+  caches.keys().then(cacheNames => {
+    return Promise.all(cacheNames.map(name => caches.delete(name)));
+  });
+  clients.claim();
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+// 不再缓存任何请求，全部从网络获取
+self.addEventListener('fetch', event => {
+  event.respondWith(fetch(event.request));
 });
